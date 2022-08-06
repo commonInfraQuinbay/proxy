1d4c3c762d77ed4c8c080d2973f14bcaf13ad983
Kanisk Gupta <kaniskgupta@Kanisks-MacBook-Pro.local>
2022-08-06 14:02:59 +0530

    initial commit

---
+++ src/test/java/com/example/commonInfraProxy/CommonInfraProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commonInfraProxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonInfraProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
